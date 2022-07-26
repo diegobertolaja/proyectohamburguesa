510277dfcf561831fff66524409271269021dbe2
diegobertolaja <diegobertolaja@yahoo.com.ar>
2022-07-25 21:30:47 -0300

    cambios

--- app/Http/Controllers/ControladorPostulacion.php
+++ app/Http/Controllers/ControladorPostulacion.php
@@ -27,7 +27,7 @@ class ControladorPostulacion extends Controller
                         $mensaje = "No tiene permisos para la operaci&oacute;n.";
                         return view('sistema.pagina-error', compact('titulo', 'codigo', 'mensaje'));
                     } else {
-                        return view('postulacion.cliente-listar', compact('titulo'));
+                        return view('postulacion.postulacion-listar', compact('titulo'));
                     }
                 } else {
                     return redirect('admin/login');
