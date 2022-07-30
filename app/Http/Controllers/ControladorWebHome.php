82e9065eb8703911bc151ce8a993c611f463f302
diegobertolaja <diegobertolaja@yahoo.com.ar>
2022-07-30 12:34:03 -0300

    cambios

--- app/Http/Controllers/ControladorWebHome.php
+++ app/Http/Controllers/ControladorWebHome.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Entidades\Sistema\Patente;
 use App\Entidades\Sistema\Usuario;
+use App\Entidades\Sucursal;
 use Session;
 
 class ControladorWebHome extends Controller
--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,12 @@ Route::group(array('domain' => '127.0.0.1'), function () {
     Route::get('/', 'ControladorWebHome@index');
     Route::get('/takeaway', 'ControladorWebTakeaway@index');
     Route::get('/nosotros', 'ControladorWebNosotros@index');
+    Route::post('/nosotros', 'ControladorWebNosotros@enviar');
     Route::get('/contacto', 'ControladorWebContacto@index');
+    Route::post('/contacto', 'ControladorWebContacto@enviar');
     Route::get('/mi-cuenta', 'ControladorWebMiCuenta@index');
+    Route::get('/login', 'ControladorWebLogin@index');
+    Route::post('/login', 'ControladorWebLogin@ingresar');
  
 
     Route::get('/admin', 'ControladorHome@index');
