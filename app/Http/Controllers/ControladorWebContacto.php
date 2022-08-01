13a396a0809cb09131300950a8d693fe4dfa00ea
diegobertolaja <diegobertolaja@yahoo.com.ar>
2022-08-01 20:48:11 -0300

    cambios

--- app/Http/Controllers/ControladorWebContacto.php
+++ app/Http/Controllers/ControladorWebContacto.php
@@ -1,11 +1,16 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Entidades\Sucursal;
 
 class ControladorWebContacto extends Controller
 {
     public function index()
     {
-            return view("web.contacto");
+        $pg = "contacto";
+            
+        $sucursal = New Sucursal();
+        $aSucursales = $sucursal-> obtenerTodos();     
+        return view("web.contacto", compact('pg', '$aSucursales'));
     }
 }
