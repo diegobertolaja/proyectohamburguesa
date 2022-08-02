795c035f38c77977c18fba3b736276ac214373b5
diegobertolaja <diegobertolaja@yahoo.com.ar>
2022-08-02 20:53:18 -0300

    cambios

--- app/Http/Controllers/ControladorWebContacto.php
+++ app/Http/Controllers/ControladorWebContacto.php
@@ -24,5 +24,25 @@ class ControladorWebContacto extends Controller
         
     }
 
+    $mail = New PHPMailer(true);
+    $mail->SMTPDebug = 0;
+    $mail->isSmtp();
+    $mail->Host = env ('MAIL_HOST');
+    $mail->SMTPAuth = tue;
+    $mail->Username = env ('MAIL_USERNAME');
+    $mail-Password = env ('MAIL_PASSWORD');
+    $mail->SMTPSecure = env ('MAIL_ENCRYPTION');
+    $mail->Port = env ('MAIL_PORT');
+
+    $mail->sentFrom(env('MAIL_FROM_ADRESS'), env('MAIL_FROM_NAME'));
+    $mail->addAdress($mail);
+    $mail->addReplyTo(env('MAIL_FROM_ADRESS'));
     
+    $mail->isHTML(true);
+    $mail->Subject = $subject;
+    $mail->Body = $body;
+    //$mail->send();
+
+    return redirect("/confirmacion-envio");  
+
 }
