eaa495782c7997cf2bbe46bbc939d10087c91918
diegobertolaja <diegobertolaja@yahoo.com.ar>
2022-08-02 18:25:02 -0300

    cambios

--- app/Http/Controllers/ControladorWebContacto.php
+++ app/Http/Controllers/ControladorWebContacto.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 use App\Entidades\Sucursal;
 use Illuminate\Http\Request;
 
-
 class ControladorWebContacto extends Controller
 {
     public function index()
@@ -12,6 +11,17 @@ class ControladorWebContacto extends Controller
         $sucursal = New Sucursal();
         $aSucursales = $sucursal-> obtenerTodos();     
         return view("web.contacto", compact('pg', '$aSucursales'));
+    }
+
+    public function enviar(Request $request) {
+        
+        $nombre = $request->input('txtNombre');
+        $telefono = $request->input('txtTelefono');
+        $mail = $request->input('txtMail');
+        $mensaje = $request->input('txtMensaje');
+
+        return redirect("/confirmacion-envio");         
+        
     }
 
     
