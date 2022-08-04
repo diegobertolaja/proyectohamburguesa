3dab6990e4e1b95def73daceb8ff30d834574ba4
diegobertolaja <diegobertolaja@yahoo.com.ar>
2022-08-04 20:18:30 -0300

    cambio

--- app/Http/Controllers/ControladorWebMiCuenta.php
+++ app/Http/Controllers/ControladorWebMiCuenta.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 use App\Entidades\Sucursal;
+use App\Entidades\Cliente;
+use App\Entidades\Pedido;
 use Illuminate\Http\Request;
+use Session;
 
 class ControladorWebMiCuenta extends Controller
 {
@@ -11,8 +14,13 @@ class ControladorWebMiCuenta extends Controller
         $pg = "mi-cuenta";
             
         $sucursal = New Sucursal();
-        $aSucursales = $sucursal-> obtenerTodos();    
+        $aSucursales = $sucursal-> obtenerTodos();  
         
-        return view("web.mi-cuenta", compact('pg', '$aSucursales'));
+        $cliente = New Cliente();
+        $cliente->obtenerPorId();    
+           
+        $pedido = New Pedido();
+        
+        return view("web.mi-cuenta", compact('pg', '$aSucursales', 'cliente'));
     }
 }
