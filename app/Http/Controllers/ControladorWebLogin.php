c572b4d9e70fd0a120b8b1768c81e04ce4adb4f3
diegobertolaja <diegobertolaja@yahoo.com.ar>
2022-08-04 17:17:21 -0300

    cambios

--- app/Http/Controllers/ControladorWebLogin.php
+++ app/Http/Controllers/ControladorWebLogin.php
@@ -2,10 +2,9 @@
 
 namespace App\Http\Controllers;
 use App\Entidades\Sucursal;
-use Entidades\Cliente;
 use Illuminate\Http\Request;
-
-
+use Entidades\Cliente;
+use Session;
 class ControladorWebLogin extends Controller
 {
     public function index()
@@ -17,24 +16,34 @@ class ControladorWebLogin extends Controller
 
             return view("web.login", compact('pg', '$aSucursales'));
     }
-  }
 
     public function ingresar (Request $request) {
-        $mail = $request->input('txtMail');
-        $clave = $request->input('txtClave');
+      $sucursal = New Sucursal();
+      $aSucursales = $sucursal-> obtenerTodos();
 
-        $cliente = New Cliente();
-        $cliente->obtenerPorMail($mail);
+      $pg = "login";
+      $mail = $request->input('txtMail');
+      $clave = $request->input('txtClave');
+
+      $cliente = New Cliente();
+      $cliente->obtenerPorMail($mail);
+
         if($cliente->idcliente > 0 && passsword_verify($clave, $cliente->clave));
-          return redirect("/mi-cuenta");
-        }   else {
-            $msg = "Usuario o clave incorrecta";
-            $sucursal = New Sucursal();
-            $aSucursales = $sucursal-> obtenerTodos();
 
-            return redirect("/web.login", compact('msg', '$aSucursales'));
+        $cliente->obtenerPorId($cliente->idcliente);
+        Session::put("idcliente", $cliente->idcliente);
+        return view ("/web.mi-cuenta", compact('cliente', '$aSucursales'));
 
-        } 
+    } else {
+        $msg["msg"]= "Correo o clave incorrecto";
+        $msg["estado"]= "danger";
+        return view ("/web.login", compact('msg', '$aSucursales', 'pg'));    
+    }
+  }
+
+
+
+      
       
         
 
