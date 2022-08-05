26b4f55e5ff6421690a30c0e7f98f5f54040551d
diegobertolaja <diegobertolaja@yahoo.com.ar>
2022-08-04 23:44:57 -0300

    cambios

--- app/Http/Controllers/ControladorWebCambiarClave.php
+++ app/Http/Controllers/ControladorWebCambiarClave.php
@@ -10,21 +10,31 @@ class ControladorWebCambiarClave extends Controller
 {
     public function index()
     {
-            $pg = "inicio";
+            $pg = "cambiar clave";
             $sucursal = New Sucursal();
             $aSucursales = $sucursal-> obtenerTodos();
             return view("web.cambiar-clave", compact('pg', '$aSucursales'));
     }
 
     public function guardar(Request $request) {
+        $pg = "cambiar clave";
+        $sucursal = New Sucursal();
+        $aSucursales = $sucursal-> obtenerTodos()
+        ;
         $clave = $request->input('txtClave');
         $reClave = $request->input('txtReClave');
 
         if($clave == $reClave){
             $cliente = new Cliente();
             $cliente->obtenerPorId(Session::get('idcliente'));
+            $cliente->clave = $clave;
+            $cliente->guardar();
+            $msg = "Clave cambiada exitosamente";
+            return view("web.cambiar-clave", compact('pg', '$aSucursales', 'msg'));
+       } else {
+        $msg = "Las claves no coinciden";
+        return view("web.cambiar-clave", compact('pg', '$aSucursales', 'msg'));
 
-
-        }
+       }
 }
 }
