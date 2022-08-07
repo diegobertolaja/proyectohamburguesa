bd467f783d23f7d84b7201e9bb48de2a0b3fab95
diegobertolaja <diegobertolaja@yahoo.com.ar>
2022-08-07 12:31:15 -0300

    cambio

--- resources/views/carrito.blade.php
+++ resources/views/carrito.blade.php
@@ -10,4 +10,38 @@
            <div class="alert alert-{{ $msg['estado'] }}" role="alert">
                   {{ $msg["msg"] }}
            </div>
-                  @endif      
+                  @endif
+                  <div class="row">
+        <div class="col-12 my-5 ">   
+            <table class="table table-hover border">
+                  <thead>
+                        <tr>
+                              <th class="lead">Imagen</th>
+                              <th class="lead">Producto</th>
+                              <th class="lead">Precio</th>
+                              <th class="lead">Cantidad</th>
+                              <th class="lead">Total</th>
+                              <th></th>
+                        </tr>
+                  </thead>
+       <tbody>
+            <?php $total = 0 ?>
+            @foreach($aCarrito_productos as $item)
+            <?php $subtotal=$item->precioproducto * $item->cantidad; ?>
+            <tr>
+                  <td><img src="/files/{{ $item->imagenproducto }}" alt="" width="100"</td>   
+                  <td>{{ $item->nombreproducto }}</td>     
+                  <td>${{ $item->number_format(precioproducto, 2, ",", ".") }}</td>  
+                  <td>{{ $item->cantidad }}</td>  
+                  <td>{{ $item->number_format($subtotal, 2, ",", ".") }}</td> 
+                  <td><i class="fa-solid fa-bnad"></i></td>    
+            </tr>
+            <?php $total+=$subtotal; ?>
+            @endforeach
+
+       </tbody>           
+            </table>    
+      <div class="float-right lead">
+            <h4>TOTAL: ${{$total}}</h4> 
+            </div>
+            </div>                    
